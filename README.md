382e4378cbe26aca9f488bc91906810d0078c48b
yurii-vyrovyi <54939587+yurii-vyrovyi@users.noreply.github.com>
2023-01-25 11:12:26 +0100

    implementation (#1)
    Co-authored-by: yvyrovyi <yvyrovyi@ingenico.com>

---
+++ README.md
@@ -0,0 +1,42 @@
+# rungroup
+
+Some services run its parts in parallel (i.e. http server, Kafka consumer). 
+Because of this:
+- there's no one blocking call in main func
+- if one of parallel run parts is down the rest should correctly close before the service goes down
+
+`rungroup` manages correct lifecycle for such cases.
+
+## How to use it?
+
+You can find a working example [here](./rungroup_test.go#L51).
+
+`rungroup` runs jobs in a separate go routine each. 
+When one of the jobs returns, `rungroup` cancels a context and waits until all jobs are finished.
+It collects all errors and returns it in one piece.
+
+**!!! Don't forget to close correctly every job.**
+It might be done in jobs' defer section or after `RunAndWait()` call.
+
+```go
+func letsRun(ctx context.Context) error{
+    runGroup := Group{}
+
+    svc1 := NewTestService("svc1", 2*time.Second)
+    svc2 := NewTestService("svc2", 0*time.Second)
+    
+    runGroup.AddJob(func(ctx context.Context) error {
+        defer func() { _ = svc1.Close() }()
+    
+        return svc1.Run(ctx)
+    })
+    
+    runGroup.AddJob(func(ctx context.Context) error {
+        defer func() { _ = svc2.Close() }()
+    
+        return svc2.Run(ctx)
+    })
+    
+    return runGroup.RunAndWait(ctx)
+}
+```
---
+++ rungroup.go
@@ -0,0 +1,69 @@
+package rungroup
+
+import (
+	"context"
+	"sync"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+type Group struct {
+	jobs []func(context.Context) error
+}
+
+func (g *Group) AddJob(job func(context.Context) error) {
+	g.jobs = append(g.jobs, job)
+}
+
+func (g *Group) AddJobs(jobs ...func(context.Context) error) {
+	g.jobs = append(g.jobs, jobs...)
+}
+
+func (g *Group) RunAndWait(ctx context.Context) error {
+	var retErr error
+
+	runCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Collecting errors until all the jobs are down
+	errChan := make(chan error)
+	wgErr := sync.WaitGroup{}
+	wgErr.Add(1)
+	go func() {
+		defer wgErr.Done()
+
+		for err := range errChan {
+			if err != nil {
+				retErr = multierror.Append(retErr, err)
+			}
+		}
+	}()
+
+	// Running jobs
+	wgRun := sync.WaitGroup{}
+	wgRun.Add(len(g.jobs))
+	for _, job := range g.jobs {
+		job := job
+
+		go func() {
+			defer wgRun.Done()
+
+			if err := job(runCtx); err != nil {
+				errChan <- err
+			}
+			cancel()
+		}()
+	}
+
+	// Waiting until all jobs are finished
+	wgRun.Wait()
+
+	// Everyone who could write to channel are down now, we can safely close the channel
+	close(errChan)
+
+	// Terminating error-collecting routine
+	wgErr.Wait()
+
+	// Voilà!
+	return retErr //nolint:wrapcheck // nothing to add
+}
